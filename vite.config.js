2b14906dea89074e849b366f5f4d2003a73babf1
TKTheProgrammer <tk.tawodzera@gmail.com>
2026-07-26 01:18:39 +0200

    Point project at new JKS_Kwekwe repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Karate_website/',
+  base: '/JKS_Kwekwe/',
   plugins: [react()],
 })
